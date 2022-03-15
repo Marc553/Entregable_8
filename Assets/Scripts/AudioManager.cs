8ac26d8daa7119b451ff3623dbe5b5f21a6191b2
Marc553 <marcramis2016@gmail.com>
2022-03-15 22:20:53 +0100

    15/03/2022

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,31 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class AudioManager : MonoBehaviour
 {
     private AudioSource audioSource;
     public AudioClip[] cancion;
+    public string[] nombre;
     
     public int cancionActual;
-
+    public TextMeshProUGUI nombreCan;
 
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
         audioSource.PlayOneShot(cancion[cancionActual]);
+        nombreCan.text = $"{nombre[cancionActual]}";
     }
 
-    void Update()
+    public void AvCan()
     {
-        
-    }
+        audioSource.Stop();
 
-    public void AvCancion()
-    {
         cancionActual++;
+        if(cancionActual == 7)
+        {
+            cancionActual = 0;
+        }
+        audioSource.PlayOneShot(cancion[cancionActual]);
+        nombreCan.text = $"{nombre[cancionActual]}";
     }
- 
 
+    public void ReCan()
+    { 
+        audioSource.Stop();
+        cancionActual--;
+        
+        if(cancionActual == -1)
+        {
+            cancionActual = 6;
+        }
+        audioSource.PlayOneShot(cancion[cancionActual]);
+        nombreCan.text = $"{nombre[cancionActual]}";
+    }
+    
+    public void Pausa()
+    {
+        audioSource.Pause();
+    }
+    public void Play()
+    {
+        audioSource.UnPause();
+    }
+    
+    public void RandomCan()
+    {
+        audioSource.Stop();
+        int randomcan = Random.Range(0, cancion.Length);
+        cancionActual = randomcan;
+        audioSource.PlayOneShot(cancion[cancionActual]);
+        nombreCan.text = $"{nombre[cancionActual]}";
 
+    }
 }
